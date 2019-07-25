dc62c3ad1f1475b9a7fc44e0f8554c1a37c003f7
Ann Nguyen <49414851+anncancode@users.noreply.github.com>
2019-07-24 23:09:29 -0400

    add ability to change size

--- src/index.js
+++ src/index.js
@@ -2,23 +2,59 @@ let sides = 16;
 
 const makeGrid = function(sides) {
   for (let i = 0; i < sides * sides; i++) {
-    const loc = document.querySelector("#grid");
     const div = document.createElement("div");
     div.classList.add("square");
-    loc.appendChild(div);
+    document.querySelector("#grid").appendChild(div);
+
+    let sizeFr = `grid-template-columns: repeat(${sides}, 1fr)`;
+    document.querySelector("#grid").style.cssText = sizeFr;
+    let squareVal = (400 / sides).toFixed(2);
+    let squareSize = `${squareVal}px`;
+    let squareCSS = `height:${squareSize};width:${squareSize}`;
+    const square = document.querySelectorAll(".square");
+    square.forEach(div => (div.style.cssText = squareCSS));
+    // this is too laggy the higher the values!!
   }
 };
 
 makeGrid(sides);
 
-const sketchPad = document.querySelector("div#grid");
-const clearButton = document.querySelector("#clear");
+// destroy grid to remake
+const killGrid = function() {
+  const square = document.querySelectorAll(".square");
+  square.forEach(div => div.parentNode.removeChild(div));
+};
 
-sketchPad.addEventListener("mouseover", e => {
-  e.target.style.backgroundColor = "red";
-});
-
-clearButton.addEventListener("click", e => {
+// erase exsiting grid
+const cleanGrid = function() {
   const square = document.querySelectorAll(".square");
   square.forEach(div => (div.style.backgroundColor = "gray"));
+};
+
+const sketchPad = document.querySelector("div#grid");
+
+// draw function
+const draw = function(color) {
+  sketchPad.addEventListener("mouseover", e => {
+    e.target.style.backgroundColor = color;
+  });
+};
+
+draw("black");
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", e => cleanGrid());
+
+const sidesButton = document.querySelector("#sides");
+sidesButton.addEventListener("click", e => {
+  sides = document.querySelector("#sidesValue").value;
+  if (sides >= 65 || sides <= 1) {
+    alert("Please submit a number between 2 or 64!");
+    document.querySelector("#sidesValue").value = 16;
+  } else {
+    document.querySelector("#test").textContent = sides;
+    killGrid();
+    makeGrid(sides);
+    cleanGrid();
+  }
 });
